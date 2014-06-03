5352d7169940a061f306d2f39fd3e0139f600a91
Pablo <pablo@Pablos-MacBook-Pro.local>
2014-06-03 18:00:01 +0200

    Initial import

---
+++ MixObjCSwift/Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Utils.swift
+//  MixObjCSwift
+//
+//  Created by Pablo on 03/06/14.
+//  Copyright (c) 2014 Pablo. All rights reserved.
+//
+
+import Foundation
+import ViewController
+
+class MyUtils : NSObject{
+
+    init(){
+        
+    }
+    
+    func byValueOrReference(num1 : Int) {
+        var a = ViewController()
+    }
+}
--- MixObjCSwift/ViewController.m
+++ MixObjCSwift/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "MixObjCSwift-Bridging-Header.h"
+
 @interface ViewController ()
             
 
@@ -17,7 +19,14 @@
             
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
